6e76a4a685d07475610d111d88c3819e8967303b
JuctTr <1301842163@qq.com>
2023-01-29 16:54:40 +0800

    feat(storage): delete

--- packages/storage/__test__/test.spec.ts
+++
@@ -1,7 +0,0 @@
-// import { describe, expect, test, vi } from 'vitest'
-// import { isPlainObject } from '../index'
-
-// test('should a plain object', async () => {
-//     const fn = vi.fn()
-//     expect(isPlainObject(fn)).toBe(false)
-// })
